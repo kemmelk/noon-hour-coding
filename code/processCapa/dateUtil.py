e3afb94a0f39a0d261e4102f1bdeff9c42c33834
Kevin Emmelkamp <KEmmelkamp@nhcweb.com>
2022-04-28 14:02:26 -0600

    Updating Scripts for the data upload

---
+++ code/processCapa/dateUtil.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 28 13:55:08 2022
+
+@author: KEmmelkamp
+"""
+
+import datetime as dt
+
+def RoundHoursUpSix(dateTime):
+    """Date utility function which rounds hours up to the next six hours
+    
+    :param dateTime: 
+    """
--- code/processCapa/main.py
+++ code/processCapa/main.py
@@ -5,14 +5,16 @@ Created on Tue Apr 26 15:12:44 2022
 
 @author: KEmmelkamp
 """
-
+import os
 import accessCaPa as acp
 
 def main():
     StartDate = "2011-04-12 00"
     EndDate = "2011-04-13 00"
-    #file_list = acp.DownloadCapaData(start_date=StartDate, end_date=EndDate)
-    #print(file_list)
+    
+    acp.DownloadCapaData(start_date=StartDate, end_date=EndDate)
+    
+    download_files = os.listdir("download_files/")
     
 if __name__ == "__main__":
     main()
